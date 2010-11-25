34bbedd87d612158fee53b00815816716c2d8560
Alon Levy <alevy@redhat.com>
2010-11-25 10:12:07 +0200

    libcaccard: dos2unix on vcard_emul_type.h

--- libcaccard/vcard_emul_type.h
+++ libcaccard/vcard_emul_type.h
@@ -1,7 +1,8 @@
 /*
- *  This header file abstracts the different card types. The goal is new card types can easily
- *  be added by simply changing this file and vcard_emul_type.c. It is currently not a requirement
- *  to dynamically add new card types.
+ *  This header file abstracts the different card types. The goal is new card
+ *  types can easily be added by simply changing this file and
+ *  vcard_emul_type.c. It is currently not a requirement to dynamically add new
+ *  card types.
  */
 
 #ifndef VCARD_EMUL_TYPE_H
@@ -19,9 +20,9 @@ typedef enum {
 } VCardEmulType;
 
 /* functions used by the rest of the emulator */
-VCardStatus vcard_init(VReader *vreader, VCard *vcard, VCardEmulType type, const char *params, 
-                       unsigned char * const *cert, int cert_len[], VCardKey *key[], 
-                       int cert_count);
+VCardStatus vcard_init(VReader *vreader, VCard *vcard, VCardEmulType type,
+                       const char *params, unsigned char * const *cert,
+                       int cert_len[], VCardKey *key[], int cert_count);
 VCardEmulType vcard_emul_type_select(VReader *vreader);
 VCardEmulType vcard_emul_type_from_string(const char *type_string);
 
